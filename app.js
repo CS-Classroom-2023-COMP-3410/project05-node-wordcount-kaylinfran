356979ae126eb8991345982cdf1cbd45f414bb2d
Kaylin Francisco <Kaylin.Francisco@du.edu>
2026-02-07 18:38:25 -0700

    Project 5 - Final Version

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 // TODO: Import required modules
 // Hint: You will need the 'fs' module for reading the file and the 'chalk' library for coloring the words.
 
+const { file } = require('@babel/types');
+
 /**
  * Synchronously reads the content of 'declaration.txt'.
  * @returns {string} The content of the file.
  */
+// TODO: Use the 'fs' module to synchronously read the content of 'declaration.txt' and return it.
+
+// import fs and chalk
+const fs = require('fs');
+const chalk = require('chalk');
+
 function readFileContent() {
-    // TODO: Use the 'fs' module to synchronously read the content of 'declaration.txt' and return it.
+    return fs.readFileSync('declaration.txt', 'utf-8');
 }
 
 /**
  * Gets the word count from the content.
  * @param {string} content The file content.
  * @returns {Object} An object with words as keys and their occurrences as values.
- */
+ */ 
 function getWordCounts(content) {
     // TODO: Implement a function to count occurrences of each word in the content.
     // Hint: Consider splitting the content into words and then tallying the counts.
     const wordCount = {};
-    const words = content.split(/\W+/).filter(Boolean); // Splitting by non-word characters.
+    const words = content.toLowerCase().split(/\W+/).filter(Boolean); // Splitting by non-word characters.
 
+    // go through each word in the file content
+    for (const word of words) {
+        if (wordCount[word]) {
+            // if word already exists in wordCount, increment the count by 1
+            wordCount[word] += 1; 
+        } else {
+            // if word is not in wordCount, count is 1
+            wordCount[word] = 1;
+        }
+    }
+    return wordCount;
 }
 
 /**
@@ -34,6 +53,13 @@ function colorWord(word, count) {
     // - Words that occur once can be blue
     // - Words that occur between 2 and 5 times can be green
     // - Words that occur more than 5 times can be red
+    if (count === 1) {
+        return chalk.blue(word);
+    } else if (count > 1 && count <= 5) {
+        return chalk.green(word);
+    } else {
+        return chalk.red(word);
+    }
 }
 
 /**
@@ -47,8 +73,12 @@ function printColoredLines(content, wordCount) {
     for (const line of lines) {
         const coloredLine = line.split(/\W+/).map(word => {
             // TODO: Color the word based on its frequency using the 'colorWord' function.
+            // convert to lowercase to match keys in wordCount
+            const lowerWord = word.toLowerCase(); 
+            // if the word exists in wordCount, color it to it's desired color, else return the original word
+            return colorWord(word, wordCount[lowerWord]);
         }).join(' ');
-
+        // print colored lines 
         console.log(coloredLine);
     }
 }
@@ -69,3 +99,9 @@ if (require.main === module) {
 
 // TODO: Export the functions for testing
 // Hint: You can use the 'module.exports' syntax.
+module.exports = {
+    readFileContent,
+    getWordCounts,
+    colorWord,
+    printColoredLines
+}
